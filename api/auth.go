23144545d58d34af89feeabe068d1181772c2f3b
Oscar Tisnado <neptali.tisnado@outlook.com>
2024-07-16 19:47:37 +0000

    add azure devops authentication

---
+++ api/auth.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"context"
+	"log"
+
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v7"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/core"
+	"github.com/otisnado/adoctl/utils"
+	"github.com/spf13/viper"
+)
+
+func CoreClient() core.Client {
+	organizationUrl := viper.GetString("organizationurl")
+	organizationToken := utils.DecodeStringBase64(viper.GetString("organizationtoken"))
+
+	connection := azuredevops.NewPatConnection(organizationUrl, organizationToken)
+
+	ctx := context.Background()
+
+	coreClient, err := core.NewClient(ctx, connection)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return coreClient
+}
