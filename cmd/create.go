67f42d5e5572b405b0eb3c0f5930410be2cf46fb
Oscar Tisnado <neptali.tisnado@outlook.com>
2024-07-18 22:57:57 +0000

    add create command to create resources in azure devops organization

---
+++ cmd/create.go
@@ -0,0 +1,24 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Command to create Azure DevOps resources",
+	Long:  `This command act like a verb to create Azure DevOps' resources.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("create called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+}
